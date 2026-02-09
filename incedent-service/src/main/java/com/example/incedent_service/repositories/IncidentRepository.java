ff17819a2bf7252475183ac2ec81278b04f64931
DeepSeeker <nlyskv12@gmail.com>
2026-02-09 18:46:22 +0300

    Изменены поиск по статусу и статус+сервис. Добавлен поиск по приоритету

--- incedent-service/src/main/java/com/example/incedent_service/repositories/IncidentRepository.java
+++ incedent-service/src/main/java/com/example/incedent_service/repositories/IncidentRepository.java
@@ -1,7 +1,6 @@
-package com.example.incedent_service.repoositories;
+package com.example.incedent_service.repositories;
 
 import com.example.incedent_service.entities.Incident;
-import com.example.incedent_service.entities.IncidentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +10,8 @@ import java.util.Optional;
 @Repository
 public interface IncidentRepository extends JpaRepository<Incident, Long> {
     Optional<Incident> findById(Long id);
-    List<Incident> findByStatus(IncidentStatus status);
+    List<Incident> findByStatus(Incident.IncidentStatus status);
     List<Incident> findByService(String service);
-    List<Incident> findByStatusAndService(IncidentStatus status, String service);
+    List<Incident> findByStatusAndService(Incident.IncidentPriority priority, String service);
+    List<Incident> findByPriority(Incident.IncidentPriority priority);
 }
