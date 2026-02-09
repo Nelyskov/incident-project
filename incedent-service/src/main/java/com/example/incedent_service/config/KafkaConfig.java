aad519de2d9bbed70b1dccb2cf390488bd63ffb1
DeepSeeker <nlyskv12@gmail.com>
2026-02-09 15:45:45 +0300

    Добавлен updateProducer в кафкаконфиг

--- incedent-service/src/main/java/com/example/incedent_service/config/KafkaConfig.java
+++ incedent-service/src/main/java/com/example/incedent_service/config/KafkaConfig.java
@@ -39,6 +39,24 @@ public class KafkaConfig {
     }
 
     @Bean
+    public ProducerFactory<String, Incident> incidentUpdatedEventProducerFactory(){
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        configProps.put("schema.registry.url", schemaRegistryUrl);
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, Incident> incidentUpdatedEventKafkaTemplate(){
+        return new KafkaTemplate<>(incidentUpdatedEventProducerFactory());
+    }
+
+    @Bean
     public KafkaTemplate<String, Incident>incidentCreatedEventKafkaTemplate(){
         return new KafkaTemplate<>(incidentCreatedEventProducerFactory());
     }
