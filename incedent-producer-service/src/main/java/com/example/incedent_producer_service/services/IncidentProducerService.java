47d1c25359de11bffa1f846ec24407db6629c3e8
DeepSeeker <nlyskv12@gmail.com>
2026-02-11 20:14:17 +0300

    добавлен метод, отправляющий ответ из бд в рест клиент и если HIGH то alert

--- incedent-producer-service/src/main/java/com/example/incedent_producer_service/services/IncidentProducerService.java
+++ incedent-producer-service/src/main/java/com/example/incedent_producer_service/services/IncidentProducerService.java
@@ -1,5 +1,7 @@
 package com.example.incedent_producer_service.services;
 
+import com.example.common.events.IncidentCreatedEvent;
+import com.example.common.events.IncidentPriority;
 import com.example.incedent_producer_service.config.KafkaConfig;
 import com.example.incedent_producer_service.entities.CreateIncidentRequest;
 import com.example.incedent_producer_service.entities.Incident;
@@ -8,28 +10,61 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class IncidentProducerService {
 
     private  final KafkaTemplate<String, Object> kafkaTemplate;
+    private final ConcurrentHashMap<String, CompletableFuture<Incident>> pendingRequests = new ConcurrentHashMap<>();
     private static final String INCIDENT_CREATE_TOPIC = "incident-create";
+    private static final String INCIDENT_RESPONSE_TOPIC = "incident-response";
     private static final String INCIDENT_UPDATE_STATUS_TOPIC = "incident-status-update";
     private static final String INCIDENT_UPDATE_PRIORITY_TOPIC = "incident-priority-update";
     private static final String INCIDENT_FIND_TOPIC = "incident-find";
     private static final String INCIDENT_HIGH_PRIORITY_ALERT = "high-priority-alert";
 
-    public Incident createIncident(CreateIncidentRequest request){
-        kafkaTemplate.send(INCIDENT_CREATE_TOPIC, request)
+    public IncidentCreatedEvent createIncident(CreateIncidentRequest request){
+        String uuid = java.util.UUID.randomUUID().toString();
+        CompletableFuture<Incident> future = new CompletableFuture<>();
+        pendingRequests.put(uuid, future);
+
+        IncidentCreatedEvent event = IncidentCreatedEvent.newBuilder()
+                .setId(0l)
+                .setService(request.getService())
+                .setInfo(request.getInfo())
+                .setService(request.getService())
+                .setPriority(request.getPriority())
+                .setTimestamp(Instant.now().toEpochMilli())
+                .build();
+        kafkaTemplate.send(INCIDENT_CREATE_TOPIC, uuid, event)
                 .whenComplete((result, ex) -> {
-                    if(ex == null){
-                        log.info("CreateIncidentRequest отправлен в incident-service");
-                    }else
+                    if(ex == null)
                     {
-                        log.info("Ошибка при отправке в incident service");
+                        log.info("Событие отправлено");
+                    }
+                    else {
+                        log.error("Ошибка при отправке");
+                        future.completeExceptionally(ex);
+                        pendingRequests.remove(uuid);
                     }
                 });
+
+        IncidentCreatedEvent incidentCreated = (IncidentCreatedEvent) future.get(30, TimeUnit.SECONDS);
+        log.info("Инцидент создан в бд");
+        return incidentCreated;
+
+
+        if(event.getPriority() == IncidentPriority.HIGH){
+            kafkaTemplate.send(INCIDENT_HIGH_PRIORITY_ALERT, event);
+        }
+
     }
 
 
